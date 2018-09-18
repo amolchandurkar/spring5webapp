0c926c7336d0898e0f505f99dcacb9ed679d0ca9
Amol Chandurkar <amol.chaandurkar@gmail.com>
2018-09-18 20:45:01 +0530

    implemented equals

--- src/main/java/com/aac/learing/spring5webapp/model/Author.java
+++ src/main/java/com/aac/learing/spring5webapp/model/Author.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 public class Author {
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
@@ -30,6 +30,31 @@ public class Author {
         this.lastName = lastName;
         this.books = books;
     }
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
     public Long getId() {
         return id;
@@ -62,4 +87,21 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Author [id=");
+		builder.append(id);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", books=");
+		builder.append(books);
+		builder.append("]");
+		return builder.toString();
+	}
+    
+    
 }
